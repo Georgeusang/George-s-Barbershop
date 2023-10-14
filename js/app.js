b12255cde8103d5027361df47df4bc2f56bfecdb
Georgeusang <georgeusanggeorge@gmail.com>
2023-10-14 12:45:46 +0100

    added more

--- js/app.js
+++ js/app.js
@@ -43,8 +43,12 @@ const swiper = new Swiper('.swiper', {
     spaceBetween: 30,
     breakpoints: {
         // when window width is >= 320px
+        320:{  slidesPerView: 1,
+          spaceBetween: 40
+        },
+
         640: {
-          slidesPerView: 2,
+          slidesPerView: 1,
           spaceBetween: 40
         },
         // when window width is >= 480px
